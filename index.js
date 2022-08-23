b814fccbbf696e4e298271805a1fc70e412e0642
Marcus Piri <Marcus.piri@cmeducations.se>
2022-08-23 18:01:41 +0200

    trying to solve corse error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const port = process.env.PORT;
 const { Server } = require("socket.io");
 const io = new Server(port, {
 	cors: {
-		origin: ["https://k1-backend-web.herokuapp.com"],
+		origin: ["https://k1-frontend-web.herokuapp.com"],
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
